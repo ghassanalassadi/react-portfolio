f73c393e8f24e8abff1e260d1bed6ab67d890bd2
ghassan <ghassanalassadi@outlook.com>
2022-10-27 21:40:04 +1100

    working to improve website

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,33 +1,27 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import portfolioData from '../portfolioData.json';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 
 function Portfolio() {
     return (
         <div id="portfolio">
-            <h1  className='d-flex justify-content-center align-items-center'>Portfolio</h1>
-            <Container  className='d-flex justify-content-center align-items-center'>
-            <Row xs={3}>
+            <h1 className='d-flex justify-content-center align-items-center'>Portfolio</h1>
+            <Container>
                 {
                 portfolioData && portfolioData.map( data => {
                     return (
                         <div key={data.id}>
-                            
-                            {data.projects && data.projects.map(projectData => {
+                            {data.projects && data.projects.map((projectData, i) => {
                                 return (
-                                    <div key={projectData.id}>
-                                        <Col sm={8} md={6} className='mt-4 mr-4 flex-wrap'>
-                                            <Card style={{ width: '18rem' }}>
+                                    <div className='projects' key={i}>
+                                                <Card style={{ width: '18rem' }} className='project-grid'>
                                                 <Card.Body>
                                                     <Card.Title><strong>{projectData.title}</strong></Card.Title>
                                                     <Card.Link href={projectData.github}>GitHub</Card.Link>
                                                 </Card.Body>
                                                 <Card.Img variant="top" src={process.env.PUBLIC_URL + projectData.img} className='card-img'/>
                                             </Card>
-                                        </Col>
                                     </div>
                                 )
                             })}
@@ -36,7 +30,6 @@ function Portfolio() {
                     )
                 })
                 }
-            </Row>
             </Container>
         </div>
     )
